4dfe3484c10eaa634360e7eb4e98c13a1a0ef88d
LeandroM27 <leandromenendez9704@outlook.com>
2024-03-21 14:32:24 -0600

    reverting changes since test was succesful

--- terraform/alb_controller.tf
+++ terraform/alb_controller.tf
@@ -1,67 +1,34 @@
-# data "aws_iam_role" "aws_load_balancer_controller" {
-#   name = "prueba-eks-alb-controller-rol-dev"
-# }
-
-
-# resource "helm_release" "aws-load-balancer-controller" {
-#   name = "prueba-eks-alb-controller-dev"
-
-#   repository = "https://aws.github.io/eks-charts"
-#   chart      = "aws-load-balancer-controller"
-#   namespace  = "default"
-#   version    = "1.4.1"
-
-#   set {
-#     name  = "clusterName"
-#     value = data.aws_eks_cluster.eks-cluster.id
-#   }
-
-#   set {
-#     name  = "image.tag"
-#     value = "v2.4.2"
-#   }
-
-#   set {
-#     name  = "serviceAccount.name"
-#     value = "prueba-eks-alb-controller-sa-dev" // creates service account
-#   }
-
-#   set {
-#     name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" // passes anotation to sa
-#     value = data.aws_iam_role.aws_load_balancer_controller.arn
-#   }
-
-# }
-
-# this is added for a test
-
-data "aws_vpc" "main" {
-  id = "vpc-072748e68eff925dc"
+data "aws_iam_role" "aws_load_balancer_controller" {
+  name = "prueba-eks-alb-controller-rol-dev"
 }
 
-resource "aws_security_group" "alb-sg" {
-  name   = "alb-sg"
-  vpc_id = data.aws_vpc.main.id
 
-  ingress {
-    protocol         = "tcp"
-    from_port        = 80
-    to_port          = 80
-    cidr_blocks      = ["0.0.0.0/0"]
+resource "helm_release" "aws-load-balancer-controller" {
+  name = "prueba-eks-alb-controller-dev"
+
+  repository = "https://aws.github.io/eks-charts"
+  chart      = "aws-load-balancer-controller"
+  namespace  = "default"
+  version    = "1.4.1"
+
+  set {
+    name  = "clusterName"
+    value = data.aws_eks_cluster.eks-cluster.id
   }
 
-  ingress {
-    protocol         = "tcp"
-    from_port        = 443
-    to_port          = 443
-    cidr_blocks      = ["0.0.0.0/0"]
+  set {
+    name  = "image.tag"
+    value = "v2.4.2"
   }
 
-  egress {
-    protocol         = "-1"
-    from_port        = 0
-    to_port          = 0
-    cidr_blocks      = ["0.0.0.0/0"]
-    
+  set {
+    name  = "serviceAccount.name"
+    value = "prueba-eks-alb-controller-sa-dev" // creates service account
   }
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn" // passes anotation to sa
+    value = data.aws_iam_role.aws_load_balancer_controller.arn
+  }
+
 }
